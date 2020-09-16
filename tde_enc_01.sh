6eee0d9202f92109b96518205c3695657b187bb6
bigs <wrlawodms@gmail.com>
2020-09-16 18:16:53 +0900

    Test for setting tde algorithm when create table

---
+++ tde_enc_01.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir $DBNAME
+cd $DBNAME
+cubrid deletedb $DBNAME
+cubrid createdb --db-volume-size=128M --log-volume-size=128M $DBNAME ko_KR.utf8
+
+cubrid server start ${DBNAME}
+
+csql -udba -c "create table tbl_no_enc (a int)" ${DBNAME}
+csql -udba -c "create table tbl_enc (a int) encrypt" ${DBNAME}
+csql -udba -c "create table tbl_enc_aes (a int) encrypt=aes" ${DBNAME}
+csql -udba -c "create table tbl_enc_aria (a int) encrypt=aria" ${DBNAME}
+
+csql -udba -c "select class_name, tde_algorithm from db_class where class_name like 'tbl_%'" ${DBNAME}
+
+cubrid server stop ${DBNAME}
+cubrid deletedb $DBNAME
