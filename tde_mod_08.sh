5804c4f16613c6819700d3941d215b3ef6625ec0
bigs <wrlawodms@gmail.com>
2020-09-16 17:28:58 +0900

    Test for recovery fail without tde module

---
+++ tde_mod_08.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir $DBNAME
+cd $DBNAME
+cubrid deletedb $DBNAME
+cubrid createdb --db-volume-size=128M --log-volume-size=128M $DBNAME ko_KR.utf8
+
+cubrid server start ${DBNAME}
+SERVER_PID=`pgrep -u $USER -f "$DBNAME$"`
+
+csql -udba -c "create table a (a int) encrypt" ${DBNAME}
+csql -udba -c "create table sp (sp char(20000))" ${DBNAME}
+csql -udba -c "insert into a (a) values(3)" ${DBNAME}
+csql -udba -c "insert into sp (sp) values(' ')" ${DBNAME}
+csql -udba -c "insert into sp (sp) values(' ')" ${DBNAME}
+
+ps aux | grep $SERVER_PID
+kill -9 $SERVER_PID
+sleep 2
+ps aux | grep $SERVER_PID
+# cubrid server stop ${DBNAME}
+
+rm ${DBNAME}_keys
+
+cubrid server start ${DBNAME}
+
+cubrid deletedb $DBNAME
