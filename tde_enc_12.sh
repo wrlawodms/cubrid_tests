8baecf7a634f4501ec82bf6e06f8e3b652415974
bigs <wrlawodms@gmail.com>
2020-09-17 16:33:37 +0900

    Add enc 11, 12

--- tde_enc_12.sh
+++ tde_enc_12.sh
@@ -9,8 +9,9 @@ cubrid createdb --db-volume-size=128M --log-volume-size=128M $DBNAME ko_KR.utf8
 
 cubrid server start ${DBNAME}
 
-csql -udba -c "create table ${TBNAME} (a int) encrypt;" ${DBNAME}
-csql -udba -c "alter table ${TBNAME} modify a int primary key;" ${DBNAME}
+csql -udba -c "create table ${TBNAME} (a int,b int) encrypt;" ${DBNAME}
+csql -udba -c "alter table ${TBNAME} modify a int unique;" ${DBNAME}
+csql -udba -c "alter table ${TBNAME} add key bk(b);" ${DBNAME}
 
 cubrid server stop ${DBNAME}
 
