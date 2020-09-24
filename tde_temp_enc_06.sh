f7b7eea42b746d3006a433572b634a4282beb29b
bigs <wrlawodms@gmail.com>
2020-09-24 18:33:02 +0900

    Add temp enc 06

---
+++ tde_temp_enc_06.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TBNAME=tbl_test
+SRC_TBNAME=tbl_test_src
+
+mkdir $DBNAME
+cd $DBNAME
+cubrid deletedb $DBNAME
+cubrid createdb --db-volume-size=128M --log-volume-size=64M $DBNAME ko_KR.utf8
+
+csql -udba -S -c "create table ${TBNAME}_enc (a int) encrypt;" $DBNAME;
+
+echo "tde_trace_debug=1" >> $DBCONF
+cubrid server start $DBNAME
+csql -udba -c "select * from ${TBNAME}_enc" $DBNAME;
+cubrid server stop $DBNAME
+
+echo "max_plan_cache_entries=0" >> $DBCONF
+
+cubrid server start $DBNAME
+csql -udba -c "select * from ${TBNAME}_enc" $DBNAME;
+cubrid server stop $DBNAME
+
+cubrid deletedb $DBNAME
