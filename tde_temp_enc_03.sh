b35ae5b1f91aba8ee95d8502ffd1c8583e0ae3c5
bigs <wrlawodms@gmail.com>
2020-09-24 16:27:08 +0900

    Add non encypted cases for temp tests

--- tde_temp_enc_03.sh
+++ tde_temp_enc_03.sh
@@ -10,13 +10,15 @@ cubrid createdb --db-volume-size=128M --log-volume-size=64M $DBNAME ko_KR.utf8
 
 csql -udba -S -c "create table $TBNAME (a int);" $DBNAME;
 csql -udba -S -c "create table ${TBNAME}_enc (a int) encrypt;" $DBNAME;
-csql -udba -S -c "create table ${SRC_TBNAME}_enc (a int) encrypt;" $DBNAME;
+
 csql -udba -S -c "insert into ${TBNAME} (a) values(3)" $DBNAME;
 csql -udba -S -c "insert into ${TBNAME}_enc (a) values(3)" $DBNAME;
 
 echo "tde_trace_debug=1" >> $DBCONF
 
 cubrid server start $DBNAME
+
+csql -udba -c "select * from ${TBNAME}" $DBNAME;
 
 csql -udba -c "select * from ${TBNAME}_enc" $DBNAME;
 csql -udba -c "select * from $TBNAME, ${TBNAME}_enc" $DBNAME;
--- tde_temp_enc_04.sh
+++ tde_temp_enc_04.sh
@@ -10,14 +10,16 @@ cubrid createdb --db-volume-size=128M --log-volume-size=64M $DBNAME ko_KR.utf8
 
 csql -udba -S -c "create table $TBNAME (a int);" $DBNAME;
 csql -udba -S -c "create table ${TBNAME}_enc (a int) encrypt;" $DBNAME;
-csql -udba -S -c "create table ${SRC_TBNAME}_enc (a int) encrypt;" $DBNAME;
+
+csql -udba -S -c "insert into ${TBNAME} (a) values(3)" $DBNAME;
 csql -udba -S -c "insert into ${TBNAME}_enc (a) values(3)" $DBNAME;
 
 echo "tde_trace_debug=1" >> $DBCONF
 
 cubrid server start $DBNAME
 
-csql -udba -c "delete ${TBNAME}_enc where a = 4" $DBNAME;
+csql -udba -c "delete ${TBNAME} where a = 3" $DBNAME;
+csql -udba -c "delete ${TBNAME}_enc where a = 3" $DBNAME;
 
 cubrid server stop $DBNAME
 
