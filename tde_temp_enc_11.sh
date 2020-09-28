1f7a6af37b8677254c4ecb5983d6ddae2a6561bb
bigs <wrlawodms@gmail.com>
2020-09-28 16:59:42 +0900

    Add temp enc 11

---
+++ tde_temp_enc_11.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+TBNAME=tbl_test
+SRC_TBNAME=tbl_test_src
+
+rm -rf $DBNAME
+mkdir $DBNAME
+cd $DBNAME
+cubrid deletedb $DBNAME
+cubrid createdb --db-volume-size=128M --log-volume-size=64M $DBNAME en_US
+
+csql -udba -S -c "create table ${TBNAME}(a char(9000));" $DBNAME;
+csql -udba -S -c "create table ${TBNAME}_enc(a char(9000)) encrypt;" $DBNAME;
+csql -udba -S -c "create table ${TBNAME}_big_enc(a char(40000)) encrypt;" $DBNAME;
+
+echo "logpb_logging_debug=1" >> $DBCONF
+echo "log_compress=0" >> $DBCONF
+
+for i in {1..3}
+do
+csql -udba -S -c "insert into ${TBNAME}_enc (a) values(' ')" $DBNAME
+done
+
+csql -udba -S -c "insert into ${TBNAME}_big_enc (a) values(' ')" $DBNAME
+
+cat csql.err | egrep -A1 -e "tde_algorithm|logpb_append_next_record";
+
+cubrid deletedb $DBNAME
