a135c911c6e27e12d7927bc40132b2a7080c4507
bigs <wrlawodms@gmail.com>
2020-09-28 11:23:59 +0900

    Add tde temp 9, 10

---
+++ tde_temp_enc_09.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+TBNAME=tbl_test
+SRC_TBNAME=tbl_test_src
+
+rm -r $DBNAME
+mkdir $DBNAME
+cd $DBNAME
+cubrid deletedb $DBNAME
+cubrid createdb --db-volume-size=128M --log-volume-size=64M $DBNAME en_US
+
+cubrid server start $DBNAME
+
+csql -udba -c "create table ${TBNAME}(a int, b int) encrypt;" $DBNAME;
+for i in {1..2}
+do
+  csql -udba -c "insert into ${TBNAME}(a, b) values (${i}, ${i}0);" $DBNAME;
+done
+
+cubrid server stop $DBNAME
+
+echo "tde_trace_debug=1" >> $DBCONF
+
+csql -udba -S -c "create index ${TBNAME}_idx on ${TBNAME} (a);" $DBNAME
+csql -udba -S -c "select * from ${TBNAME} order by b;" $DBNAME
+csql -udba -S -c "select avg(a), b from ${TBNAME} group by b;" $DBNAME
+csql -udba -S -c "select avg(a) over (partition by b) from ${TBNAME};" $DBNAME
+csql -udba -S -c "select avg(a), b from ${TBNAME} group by b;" $DBNAME
+
+cubrid deletedb $DBNAME
