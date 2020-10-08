c3e95d2dda451e74b86071f30445babfbf6e5b38
bigs <wrlawodms@gmail.com>
2020-10-08 17:51:58 +0900

    Add utils 9

---
+++ tde_utils_09.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TBNAME=test_tbl
+
+mkdir $DBNAME
+cd $DBNAME
+cubrid deletedb $DBNAME
+
+cubrid createdb --db-volume-size=128M --log-volume-size=128M $DBNAME en_US
+
+echo "tde_default_algorithm=ARIA" >> $DBCONF
+
+cubrid paramdump -S $DBNAME | egrep -e "tde_keys_file_path|tde_default_algorithm"
+
+cubrid deletedb $DBNAME
