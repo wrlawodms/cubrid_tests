4d69d0a8e4bc4b07f7ab6b062b4a1ff266391388
bigs <wrlawodms@gmail.com>
2020-10-08 16:49:47 +0900

    Add utils 2, 3

---
+++ tde_utils_03.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+TBNAME=test_tbl
+COPY_DBNAME=${DBNAME}_copy
+KEYS_PATH=keys
+
+cubrid deletedb $DBNAME
+cubrid deletedb $COPY_DBNAME
+
+rm -rf $DBNAME
+
+mkdir $DBNAME
+cd $DBNAME
+
+cubrid createdb --db-volume-size=128M --log-volume-size=128M $DBNAME en_US
+
+set -x
+# copy the key file to another directory
+mkdir $KEYS_PATH
+mv ${DBNAME}_keys ${KEYS_PATH}/
+cd $KEYS_PATH
+echo "tde_keys_file_path=`pwd`" >> $DBCONF
+cd ..
+
+csql -udba -S -c "create table $TBNAME (a int) encrypt" $DBNAME
+csql -udba -S -c "insert into $TBNAME (a) values(3)" $DBNAME
+
+mkdir $COPY_DBNAME
+cd $COPY_DBNAME
+cubrid copydb $DBNAME $COPY_DBNAME
+
+csql -udba -S -c "select class_name, tde_algorithm from db_class where class_name='$TBNAME'" $COPY_DBNAME # Check if the tde info is copied properly
+csql -udba -S -c "select * from $TBNAME" $COPY_DBNAME # Check if it can access encrypted table
+
+cd ..
+ls $COPY_DBNAME # there has to be no key file
+ls $KEYS_PATH   # there has to be copied key file
+
+cubrid deletedb $DBNAME
+cubrid deletedb $COPY_DBNAME
