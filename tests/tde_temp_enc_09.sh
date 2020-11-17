20c1fc50566300fc8144a3a4929a3defbeffb5c8
bigs <wrlawodms@gmail.com>
2020-11-17 15:51:25 +0900

    Revise temp 9

--- tests/tde_temp_enc_09.sh
+++ tests/tde_temp_enc_09.sh
@@ -9,10 +9,16 @@ cubrid createdb --db-volume-size=128M --log-volume-size=64M $DBNAME en_US
 
 cubrid server start $DBNAME
 
-csql -udba -c "create table ${TBNAME}(a int, b int) encrypt;" $DBNAME;
+csql -udba -c "create table ${TBNAME}(a int, b int);" $DBNAME;
 for i in {1..2}
 do
   csql -udba -c "insert into ${TBNAME}(a, b) values (${i}, ${i}0);" $DBNAME;
+done
+
+csql -udba -c "create table ${TBNAME}_enc(a int, b int) encrypt;" $DBNAME;
+for i in {1..2}
+do
+  csql -udba -c "insert into ${TBNAME}_enc(a, b) values (${i}, ${i}0);" $DBNAME;
 done
 
 cubrid server stop $DBNAME
@@ -22,8 +28,16 @@ csql -udba -S -c "select avg(a), b from ${TBNAME} group by b;" $DBNAME          
 csql -udba -S -c "select avg(a) over (partition by b) from ${TBNAME};" $DBNAME    # analytics
 csql -udba -S -c "create index ${TBNAME}_idx on ${TBNAME} (a);" $DBNAME           # load index
 
+csql -udba -S -c "select * from ${TBNAME}_enc order by b;" $DBNAME                    # order by
+csql -udba -S -c "select avg(a), b from ${TBNAME}_enc group by b;" $DBNAME            # group by
+csql -udba -S -c "select avg(a) over (partition by b) from ${TBNAME}_enc;" $DBNAME    # analytics
+csql -udba -S -c "create index ${TBNAME}_enc_idx on ${TBNAME}_enc (a);" $DBNAME           # load index
+
 cat csql.err | grep "TDE:" | egrep -e "sort_listfile"
 # EXPECTED:
-# TDE: sort_listfile(): tde_encrypted = 1, 1, 1, 1
+# for a normal table: 
+#   TDE: sort_listfile(): tde_encrypted = 0, 0, 0, 0
+# for an encrypted table:
+#   TDE: sort_listfile(): tde_encrypted = 1, 1, 1, 1
 
 cubrid deletedb $DBNAME
