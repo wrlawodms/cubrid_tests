f17e0eacca274588fb6da7428cf6a6ac9c13ac0f
bigs <wrlawodms@gmail.com>
2020-11-17 15:57:41 +0900

    Fix keys 12

--- tests/tde_keys_12.sh
+++ tests/tde_keys_12.sh
@@ -5,6 +5,6 @@ cubrid createdb --db-volume-size=128M --log-volume-size=128M $DBNAME en_US
 # EXPECTED: All the commands below fail, which print usage.
 cubrid tde $DBNAME # one operation is required
 cubrid tde -d $DBNAME # -d requires a key index
-cubrid tde -c $DBNAMe # -c requires a key index
+cubrid tde -c $DBNAME # -c requires a key index
 
 cubrid deletedb $DBNAME
