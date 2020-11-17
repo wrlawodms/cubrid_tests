fc7bec52505b3c5aa191541cf10fa0c8cdeacbce
bigs <wrlawodms@gmail.com>
2020-11-17 15:33:43 +0900

    Revise tbl 05

--- tests/tde_tbl_enc_05.sh
+++ tests/tde_tbl_enc_05.sh
@@ -2,6 +2,8 @@
 
 cubrid createdb --db-volume-size=128M --log-volume-size=128M $DBNAME ko_KR.utf8
 
+echo "log_compress=0" >> $DBCONF
+
 cubrid server start ${DBNAME}
 
 csql -udba -c "create table a (a int) encrypt;" ${DBNAME}
