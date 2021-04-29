604e0d4a1f8670c6624a1eb38c2e1887024e15b5
bigs <wrlawodms@gmail.com>
2021-04-29 13:43:41 +0900

    Revise the expected result of spec_02.sh

--- enhance_truncate/spec-change/spec_02.sh
+++ enhance_truncate/spec-change/spec_02.sh
@@ -12,9 +12,10 @@ cubrid server stop $DBNAME
 #   TRUNCATE when there is a FK with ON DELETE RESTRICT, NO ACTION or SET NULL action.
 
 # Expected:
-# 1. all statements fail.
-#     - the first one: ERROR: Cannot truncate the table due to the primary key referred to on fk_child_a. Try again with CASCADE option.
-#     - the others:    ERROR: Cannot cascade truncate because the ON DELETE action of the foreign key (fk_child_a) is not set to CASCADE.
+# 1. all TRUNCATEs fail.
+#     - Error messages is printed alternately:
+#         - ERROR: Cannot truncate the table due to the primary key referred to on fk_child_a. Try again with CASCADE option.
+#         - ERROR: Cannot cascade truncate because the ON DELETE action of the foreign key (fk_child_a) is not set to CASCADE.
 # 2. all select count(*) prints 1
 
 cubrid deletedb $DBNAME
