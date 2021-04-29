feb3585d26f0ec08a9895e9b7ab3e4b7310e16b6
bigs <wrlawodms@gmail.com>
2021-04-29 13:18:59 +0900

    Add cascade 02

---
+++ enhance_truncate/spec-change/cascade_02.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cubrid createdb --db-volume-size=128M --log-volume-size=128M $DBNAME en_US
+
+cubrid server start $DBNAME
+
+csql -udba -i cascade_02.sql $DBNAME
+
+cubrid server stop $DBNAME
+
+# Descripttion:
+#   CASCASDE fails if a grand-child (more 2 depth) doesn't have ON DELETE CASCADE option
+#   case: A <- B <- E, F (ON DELETE RESTRICT)
+#            L C <- G, H
+
+# Expected:
+# 1. The TRUNCATE fails, and follwing select count(*) print 1
+#     - ERROR: Cannot cascade truncate because the ON DELETE action of the foreign key (fk_grand_child2_b) is not set to CASCADE.
+cubrid deletedb $DBNAME
