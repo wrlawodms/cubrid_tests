047144640e6902958cb4154b0d3ee4792f0e0e6b
bigs <wrlawodms@gmail.com>
2021-04-29 14:46:32 +0900

    Add cascade 4

---
+++ enhance_truncate/spec-change/cascade_04.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cubrid createdb --db-volume-size=128M --log-volume-size=128M $DBNAME en_US
+
+cubrid server start $DBNAME
+
+csql -udba -i cascade_04.sql $DBNAME
+
+cubrid server stop $DBNAME
+
+# Descripttion:
+#   CASCASDE succeeds with partitioned tables
+#   case: A <- B (partitioned) <- C(partitioned)
+
+# Expected:
+# 1. The TRUNCATE succeeds, and following select count(*) print 0
+
+cubrid deletedb $DBNAME
