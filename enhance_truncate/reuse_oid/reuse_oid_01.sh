577c98c69f092b1ed587d54d2b954baebff04f1d
bigs <wrlawodms@gmail.com>
2021-05-13 10:43:43 +0900

    Revise reuse_oid 01

--- enhance_truncate/reuse_oid/reuse_oid_01.sh
+++ enhance_truncate/reuse_oid/reuse_oid_01.sh
@@ -13,7 +13,12 @@ VFID_BEFORE=`cat ${DBNAME}_diag_d1_1 | grep -B 13 "tbl" | grep vfid`
 VFID_AFTER=`cat ${DBNAME}_diag_d1_2 | grep -B 13 "tbl" | grep vfid`
 
 echo "-------Expected 2: different VFID -------"
-echo $VFID_BEFORE $VFID_AFTER # Expected 2: different
+echo "Before: $VFID_BEFORE, After: $VFID_AFTER" # Expected 2: different"
+
+echo "-------Expected 3: can't find the previous VFID -------"
+cat ${DBNAME}_diag_d1_2 | grep "$VFID_BEFORE"  # can't find
+
+rm ${DBNAME}_diag_d1_*
 
 # Descripttion:
 # TRUNCATE on a table with REUSE_OID make it have a new heap file.
