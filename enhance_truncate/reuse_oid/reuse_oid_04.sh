3a41ad4f4b0d2d333f119797ca628831c9518b91
bigs <wrlawodms@gmail.com>
2021-05-13 14:50:57 +0900

    Separate CREATE TABLE from .sql

--- enhance_truncate/reuse_oid/reuse_oid_04.sh
+++ enhance_truncate/reuse_oid/reuse_oid_04.sh
@@ -4,6 +4,7 @@ cubrid createdb --db-volume-size=128M --log-volume-size=128M $DBNAME en_US
 
 cubrid server start $DBNAME
 
+csql -udba -c "create table tbl (a int primary key)" $DBNAME
 csql -udba -i reuse_oid_04.sql $DBNAME
 
 cubrid server stop $DBNAME
--- enhance_truncate/reuse_oid/reuse_oid_04.sql
+++ enhance_truncate/reuse_oid/reuse_oid_04.sql
@@ -1,8 +1,7 @@
-create table tbl (a int primary key);
 insert into tbl values (3), (4);
 update statistics on tbl;
 ;info stats tbl
 truncate tbl;
 insert into tbl values (1); -- without this, it is considered doubtful statistic (0 objects)
 update statistics on tbl;
-;info stats tbl -- total object = 1 
+;info stats tbl -- expected: total object = 1 
