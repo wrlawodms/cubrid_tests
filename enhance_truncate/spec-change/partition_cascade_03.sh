c8ecbe0cccb82349d9460bb1c669b137f966af6d
bigs <wrlawodms@gmail.com>
2021-05-14 16:36:48 +0900

    Add part cascade 03

---
+++ enhance_truncate/spec-change/partition_cascade_03.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cubrid createdb --db-volume-size=128M --log-volume-size=128M $DBNAME en_US
+
+cubrid server start $DBNAME
+
+csql -udba -i partition_cascade_03.sql $DBNAME
+
+cubrid server stop $DBNAME
+
+# Descripttion:
+#   CASCASDE succeeds if there is a FK chain
+#   case: A <- B <- C
+#         L_________^
+
+# Expected:
+# 1. The TRUNCATE succeeds, and follwing select count(*) print 0
+
+cubrid deletedb $DBNAME
