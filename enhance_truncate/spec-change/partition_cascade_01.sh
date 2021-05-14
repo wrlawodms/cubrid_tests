e544f0445f0befa3e3dc1fcc40c98afff8b4be48
bigs <wrlawodms@gmail.com>
2021-05-14 15:58:43 +0900

    Add part cascade 01

---
+++ enhance_truncate/spec-change/partition_cascade_01.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cubrid createdb --db-volume-size=128M --log-volume-size=128M $DBNAME en_US
+
+cubrid server start $DBNAME
+
+csql -udba -i partition_cascade_01.sql $DBNAME
+
+cubrid server stop $DBNAME
+
+# Descripttion:
+#   CASCASDE succeeds even if a FK-child has a FK-child (2-depth)
+#   case: A <- B <- E, F
+#            L C <- G, H
+
+# Expected:
+# 1. The first TRUNCATE fails, and follwing select count(*) print 4
+# 2. The second TRUNCATE succeeds, and follwing select count(*) print 0
+cubrid deletedb $DBNAME
