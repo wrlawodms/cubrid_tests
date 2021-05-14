dbe9a73aec328b6b9d9f6a5d65bc8277c5843210
bigs <wrlawodms@gmail.com>
2021-05-14 16:54:10 +0900

    typo

--- enhance_truncate/reuse_oid/reuse_oid_05.sh
+++ enhance_truncate/reuse_oid/reuse_oid_05.sh
@@ -15,11 +15,11 @@ VFIDS_AFTER=`cubrid diagdb -d1 $DBNAME | grep -B 13 "tbl" | grep vfid`
 echo "Previous VFIDs: $VFIDS_BEFORE" 
 echo "Current VFIDs: $VFIDS_AFTER"
 
-csql -udba -S -c "select * from tbl" $DBNAME # Expected 2: onlt one record : a=0
+csql -udba -S -c "select * from tbl" $DBNAME # Expected 2: only one record : a=0
 
 csql -udba -S -c "create table tbl2 under tbl" $DBNAME # In this case, index is not dropped.
 csql -udba -S -i reuse_oid_05.sql $DBNAME
-csql -udba -S -c "select * from tbl" $DBNAME # Expected 3: onlt one record : a=0
+csql -udba -S -c "select * from tbl" $DBNAME # Expected 3: only one record : a=0
 
 # Descripttion:
 # Check if it is consistent after ROLLBACK. The heap and btree have to be the same as before ROLLBACK 
