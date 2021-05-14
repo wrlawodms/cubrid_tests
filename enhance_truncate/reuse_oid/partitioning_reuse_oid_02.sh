d45c8b0623b9fc7bbefec30de26f137f530f7fb3
bigs <wrlawodms@gmail.com>
2021-05-14 15:44:13 +0900

    Revise part 02

--- enhance_truncate/reuse_oid/partitioning_reuse_oid_02.sh
+++ enhance_truncate/reuse_oid/partitioning_reuse_oid_02.sh
@@ -5,15 +5,15 @@ cubrid createdb --db-volume-size=128M --log-volume-size=128M $DBNAME en_US
 cubrid server start $DBNAME
 
 csql -udba -c "create table tbl (a int) partition by hash (a) partitions 2" $DBNAME
-csql -udba -c "insert into tbl values (3)" $DBNAME
+csql -udba -c "insert into tbl values (0), (1), (2), (3)" $DBNAME
 
 csql -udba -c "truncate tbl" $DBNAME
 
 csql -udba -c "alter table tbl add column b int;" $DBNAME
-csql -udba -c "insert into tbl values(0,0)" $DBNAME
+csql -udba -c "insert into tbl values(0,0), (1,1), (2,2), (3,3)" $DBNAME
 
-echo "---------- Expected 1: (0,0) -----------"
-csql -udba -c "select * from tbl" $DBNAME # Expected 1: (0,0);
+echo "---------- Expected 1: (0,0), (1,1), (2,2), (3,3) -----------"
+csql -udba -c "select * from tbl" $DBNAME # Expected 1: (0,0), (1,1), (2,2), (3,3);
 
 cubrid server stop $DBNAME
 
