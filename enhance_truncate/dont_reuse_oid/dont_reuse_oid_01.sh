ee9dd4f4fcead3c78b7732ff07efd0109ac5b34b
bigs <wrlawodms@gmail.com>
2021-06-21 15:17:01 +0900

    Add dont_reuse_oid 1,5

---
+++ enhance_truncate/dont_reuse_oid/dont_reuse_oid_01.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cubrid createdb --db-volume-size=128M --log-volume-size=128M $DBNAME en_US
+
+csql -udba -S -c "create table tbl (a int) dont_reuse_oid" $DBNAME
+
+csql -udba -S -c "insert into tbl values(3)" $DBNAME
+echo "-------Expected 1: count(*): 1 -------"
+csql -udba -S -c "select count(*) from tbl" $DBNAME # Expected 1: 1
+
+cubrid diagdb -d1 $DBNAME > ${DBNAME}_diag_d1_1
+
+csql -udba -S -c "truncate tbl" $DBNAME
+echo "-------Expected 2: count(*): 0 -------"
+csql -udba -S -c "select count(*) from tbl" $DBNAME  # Expected 2: 0 
+
+cubrid diagdb -d1 $DBNAME > ${DBNAME}_diag_d1_2
+
+VFID_BEFORE=`cat ${DBNAME}_diag_d1_1 | grep -B 13 "tbl" | grep vfid`
+VFID_AFTER=`cat ${DBNAME}_diag_d1_2 | grep -B 13 "tbl" | grep vfid`
+
+echo "-------Expected 3: different VFID -------"
+echo "Before: $VFID_BEFORE, After: $VFID_AFTER" # Expected 3: different"
+
+echo "-------Expected 4: can't find the previous VFID -------"
+cat ${DBNAME}_diag_d1_2 | grep "$VFID_BEFORE"  # Expected 4: can't find
+
+rm ${DBNAME}_diag_d1_*
+
+# Descripttion:
+# TRUNCATE on a table with DONT_REUSE_OID make it have a new heap file.
+
+# Expected:
+# 2 expected results. See each comments above
+
+cubrid deletedb $DBNAME
---
+++ enhance_truncate/dont_reuse_oid/dont_reuse_oid_05.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cubrid createdb --db-volume-size=128M --log-volume-size=128M $DBNAME en_US
+
+csql -udba -S -c "create table tbl (a int primary key) dont_reuse_oid" $DBNAME
+csql -udba -S -c "create table tbl2 (a int foreign key references tbl(a) on delete cascade) dont_reuse_oid" $DBNAME
+csql -udba -S -c "insert into tbl values (0)" $DBNAME
+csql -udba -S -c "insert into tbl2 values (0)" $DBNAME
+
+VFIDS_BEFORE=`cubrid diagdb -d1 $DBNAME | grep -B 13 "tbl" | grep vfid` 
+
+csql -udba -S -i reuse_oid_05.sql $DBNAME
+
+VFIDS_AFTER=`cubrid diagdb -d1 $DBNAME | grep -B 13 "tbl" | grep vfid`
+
+# Expected 1: these two below have to be same"
+echo "== Expected 1: these two below have to be same =="
+echo "Previous VFIDs: $VFIDS_BEFORE" 
+echo "Current VFIDs: $VFIDS_AFTER"
+
+echo "== Expected 2: only one record : a=0 =="
+csql -udba -S -c "select * from tbl" $DBNAME # Expected 2: only one record : a=0
+
+csql -udba -S -c "create table tbl3 under tbl dont_reuse_oid" $DBNAME # In this case, index is not dropped.
+csql -udba -S -i reuse_oid_05.sql $DBNAME
+
+echo "== Expected 3: only one record : a=0 =="
+csql -udba -S -c "select * from tbl" $DBNAME # Expected 3: only one record : a=0
+
+# Descripttion:
+# Check if it is consistent after ROLLBACK. The heap and btree have to be the same as before ROLLBACK 
+
+# Expected:
+# 3 epxected reults. See comments above
+
+cubrid deletedb $DBNAME
