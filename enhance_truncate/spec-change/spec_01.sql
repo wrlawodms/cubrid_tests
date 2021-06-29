b871a357e7851e01028a0c7a1ffa3d8802c3f870
bigs <wrlawodms@gmail.com>
2021-06-29 10:51:51 +0900

    Simplify sepc 01

--- enhance_truncate/spec-change/spec_01.sql
+++ enhance_truncate/spec-change/spec_01.sql
@@ -2,29 +2,14 @@ create table tbl (a int);
 insert into tbl values(3);
 truncate tbl;
 select count(*) from tbl;
-drop tbl;
-
-create table tbl (a int primary key);
-insert into tbl values(3);
-truncate tbl;
-select count(*) from tbl;
-drop tbl;
-
-create table tbl (a int primary key);
-alter table tbl add column b int references tbl(a);
-insert into tbl values(3,3);
-truncate tbl;
-select count(*) from tbl;
-drop tbl;
-
-create table tbl (a int);
-insert into tbl values(3);
 truncate tbl cascade;
 select count(*) from tbl;
 drop tbl;
 
 create table tbl (a int primary key);
 insert into tbl values(3);
+truncate tbl;
+select count(*) from tbl;
 truncate tbl cascade;
 select count(*) from tbl;
 drop tbl;
@@ -32,6 +17,8 @@ drop tbl;
 create table tbl (a int primary key);
 alter table tbl add column b int references tbl(a);
 insert into tbl values(3,3);
+truncate tbl;
+select count(*) from tbl;
 truncate tbl cascade;
 select count(*) from tbl;
 drop tbl;
